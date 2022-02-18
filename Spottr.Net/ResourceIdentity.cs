f70d70724e98b831feab50f77bd2bade7c1ef9d6
tacosontitan <jkdavis1691@gmail.com>
2022-02-17 19:22:45 -0600

    Added id and display name.

--- Spottr.Net/ResourceIdentity.cs
+++ Spottr.Net/ResourceIdentity.cs
@@ -5,4 +5,20 @@ namespace Spottr.Net;
 /// </summary>
 public class ResourceIdentity {
     
+
+    #region Properties
+
+    /// <summary>
+    /// The unique identifier for this resource in your systems.
+    /// </summary>
+    /// <value></value>
+    public object? Id { get; set; } = null;
+    /// <summary>
+    /// The display name that should be utilized in Spottr's user interface.
+    /// </summary>
+    /// <value></value>
+    public string? DisplayName { get; set; } = null;
+
+    #endregion
+
 }
