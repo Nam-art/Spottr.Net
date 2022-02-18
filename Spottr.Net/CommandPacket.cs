f23395068f3207a5f7617485c6bab8887efd1802
tacosontitan <jkdavis1691@gmail.com>
2022-02-17 19:37:11 -0600

    Implemented IDisplayable<T>

--- Spottr.Net/CommandPacket.cs
+++ Spottr.Net/CommandPacket.cs
@@ -3,19 +3,13 @@ namespace Spottr.Net;
 /// <summary>
 /// Represents a <see cref="CommandPacket"/> for consumption in Spottr.
 /// </summary>
-public class CommandPacket : SpottrPacket
+public class CommandPacket : SpottrPacket, IDisplayable<Guid?>
 {
 
     #region Properties
 
-    /// <summary>
-    /// The unique runtime identifier for the command this packet represents.
-    /// </summary>
-    public Guid? Id { get; set; } = null;
-    /// <summary>
-    /// The name used to display the command in Spottr.
-    /// </summary>
-    public string? DisplayName { get; set; } = null;
+    public Guid? Id { get; set; }
+    public string? DisplayName { get; set; }
 
     #endregion
 
