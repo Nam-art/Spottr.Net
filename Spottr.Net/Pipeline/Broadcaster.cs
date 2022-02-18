f6d36fb8b4f330b554f8a3af7c367481131079b2
tacosontitan <jkdavis1691@gmail.com>
2022-02-17 19:45:55 -0600

    Implemented IDisplayable<T>

--- Spottr.Net/Pipeline/Broadcaster.cs
+++ Spottr.Net/Pipeline/Broadcaster.cs
@@ -3,6 +3,13 @@ namespace Spottr.Net.Pipeline;
 /// <summary>
 /// Represents a <see cref="Broadcaster"/> within Spottr enabled communication systems.
 /// </summary>
-public class Broadcaster {
+public class Broadcaster : IDisplayable<Guid?> {
+
+    #region IDisplayable<T> Implementation
+
+    public Guid? Id { get; set; }
+    public string? DisplayName { get; set; }
+
+    #endregion
 
 }
