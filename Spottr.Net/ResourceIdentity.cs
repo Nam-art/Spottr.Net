586afb99e0d27f6b0d35a740b1b9d566c1a69faa
tacosontitan <jkdavis1691@gmail.com>
2022-02-17 19:35:25 -0600

    Implemented IDisplayable<T>

--- Spottr.Net/ResourceIdentity.cs
+++ Spottr.Net/ResourceIdentity.cs
@@ -3,20 +3,11 @@ namespace Spottr.Net;
 /// <summary>
 /// Represents a <see cref="ResourceIdentity"/> to enable identification of resources within Spottr.
 /// </summary>
-public class ResourceIdentity {
-    
+public class ResourceIdentity : IDisplayable<object> {
 
-    #region Properties
+    #region IDisplayable Implementation
 
-    /// <summary>
-    /// The unique identifier for this resource in your systems.
-    /// </summary>
-    /// <value></value>
     public object? Id { get; set; } = null;
-    /// <summary>
-    /// The display name that should be utilized in Spottr's user interface.
-    /// </summary>
-    /// <value></value>
     public string? DisplayName { get; set; } = null;
 
     #endregion
