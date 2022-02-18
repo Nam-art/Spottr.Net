054f4e551ac2da6b77edfa106f06b9b1bee68c9c
tacosontitan <jkdavis1691@gmail.com>
2022-02-17 19:30:21 -0600

    Created the IDisplayable interface shell.

---
+++ Spottr.Net/IDisplayable.cs
@@ -0,0 +1,8 @@
+namespace Spottr.Net;
+
+/// <summary>
+/// Represents an <see cref="IDisplayable"/> to enable display of an object with Spottr's user interface.
+/// </summary>
+public interface IDisplayable {
+
+}
