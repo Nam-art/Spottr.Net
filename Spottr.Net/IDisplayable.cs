0c54f654b37f43b50a65ec27c82e4b8a220d0c71
tacosontitan <jkdavis1691@gmail.com>
2022-02-17 19:34:21 -0600

    Added id and display name.

--- Spottr.Net/IDisplayable.cs
+++ Spottr.Net/IDisplayable.cs
@@ -1,8 +1,22 @@
 namespace Spottr.Net;
 
 /// <summary>
-/// Represents an <see cref="IDisplayable"/> to enable display of an object with Spottr's user interface.
+/// Represents an <see cref="IDisplayable{T}"/> to enable display of an object with Spottr's user interface.
 /// </summary>
-public interface IDisplayable {
+/// <typeparam name="T">The type of data utilized to represent the identifier of the <see cref="IDisplayable{T}" />.</typeparam>
+public interface IDisplayable<T> {
+
+    #region Properties
+
+    /// <summary>
+    /// The unique identifier representing this object.
+    /// </summary>
+    T? Id { get; set; }
+    /// <summary>
+    /// The display name that should be utilized in Spottr's user interface.
+    /// </summary>
+    string? DisplayName { get; set; }
+
+    #endregion
 
 }
