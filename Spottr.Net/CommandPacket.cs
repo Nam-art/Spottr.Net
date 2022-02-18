4d01ac28d6850cb5158bad7686d8291f1947d661
tacosontitan <jkdavis1691@gmail.com>
2022-02-17 18:22:41 -0600

    Added id and display name properties.

--- Spottr.Net/CommandPacket.cs
+++ Spottr.Net/CommandPacket.cs
@@ -6,4 +6,17 @@ namespace Spottr.Net;
 public class CommandPacket
 {
 
+    #region Properties
+
+    /// <summary>
+    /// The unique runtime identifier for the command this packet represents.
+    /// </summary>
+    public Guid? Id { get; set; }
+    /// <summary>
+    /// The name used to display the command in Spottr.
+    /// </summary>
+    public string? DisplayName { get; set; }
+
+    #endregion
+
 }
