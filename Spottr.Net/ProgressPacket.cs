ee6d1e418955c8c7b6fef1833062a7cf863089f6
tacosontitan <jkdavis1691@gmail.com>
2022-02-17 18:17:35 -0600

    Added is and of properties.

--- Spottr.Net/ProgressPacket.cs
+++ Spottr.Net/ProgressPacket.cs
@@ -6,4 +6,17 @@ namespace Spottr.Net;
 public class ProgressPacket
 {
 
+    #region Properties
+
+    /// <summary>
+    /// The number of "things" that have been successfully processed.
+    /// </summary>
+    public int Is { get; set; }
+    /// <summary>
+    /// The number of "things" that are expected to be processed.
+    /// </summary>
+    public int Of { get; set; }
+
+    #endregion
+
 }
