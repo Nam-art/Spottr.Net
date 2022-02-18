e3eac96933ccb66e04398e90bc481b09eeacd100
tacosontitan <jkdavis1691@gmail.com>
2022-02-17 19:05:42 -0600

    Created resource identity shell.

---
+++ Spottr.Net/ResourceIdentity.cs
@@ -0,0 +1,8 @@
+namespace Spottr.Net;
+
+/// <summary>
+/// Represents a <see cref="ResourceIdentity"/> to enable identification of resources within Spottr.
+/// </summary>
+public class ResourceIdentity {
+    
+}
