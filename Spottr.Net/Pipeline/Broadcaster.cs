ad3d3b50c326a7ae7c21655a3c1d7c23ba74f174
tacosontitan <jkdavis1691@gmail.com>
2022-02-17 19:43:57 -0600

    Created a broadcaster shell.

---
+++ Spottr.Net/Pipeline/Broadcaster.cs
@@ -0,0 +1,8 @@
+namespace Spottr.Net.Pipeline;
+
+/// <summary>
+/// Represents a <see cref="Broadcaster"/> within Spottr enabled communication systems.
+/// </summary>
+public class Broadcaster {
+
+}
