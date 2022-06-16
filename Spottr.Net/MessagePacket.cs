febac4fedad148c1c4ec51ea074e8d0aee5f8ff8
tacosontitan <jdavis@globallcs.com>
2022-06-16 11:02:08 -0500

    Fleshed out message packet.

--- Spottr.Net/MessagePacket.cs
+++ Spottr.Net/MessagePacket.cs
@@ -5,4 +5,18 @@
 /// </summary>
 public class MessagePacket : SpottrPacket {
 
+    #region Properties
+
+    public string? Message { get; set; } = null;
+
+    #endregion
+
+    #region Constructor
+
+    public MessagePacket(string message, ResourceIdentity creator) :
+        base(DateTime.Now, creator) =>
+            Message = message;
+
+    #endregion
+
 }
