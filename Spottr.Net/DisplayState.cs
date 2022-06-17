52d251890c6f7a07b2a90b91e3c482c7968d1da1
tacosontitan <jdavis@globallcs.com>
2022-06-17 08:36:07 -0500

    Added display state enum.

---
+++ Spottr.Net/DisplayState.cs
@@ -0,0 +1,27 @@
+namespace Spottr.Net.Packets;
+
+/// <summary>
+/// Represents a <see cref="DisplayState"/> which enables Spottr to categorize information visually.
+/// </summary>
+public enum DisplayState {
+    /// <summary>
+    /// No state provided.
+    /// </summary>
+    /// <remarks>This state typically represents informational data with no display preference.</remarks>
+    None = 0,
+    /// <summary>
+    /// This state is displayed in Spottr's negative category.
+    /// </summary>
+    /// <remarks>This state typically represents errors, exceptions, etc.</remarks>
+    Negative = 1,
+    /// <summary>
+    /// This state is displayed in Spottr's negative category.
+    /// </summary>
+    /// <remarks>This state typically represents warnings.</remarks>
+    Neutral = 2,
+    /// <summary>
+    /// This state is displayed in Spottr's positive category.
+    /// </summary>
+    /// <remarks>This state typically represents successful processing.</remarks>
+    Positive = 3
+}
